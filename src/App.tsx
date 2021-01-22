fc9b90e2280c2b93446bad63b7702ee6365d7fab
Michał Łęcicki <mlecicki@gmail.com>
2021-01-22 11:05:38 +0100

    First configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+import { Button } from '@chakra-ui/react';
+
+function App({ title }: { title: string }) {
+    return (
+          <div className={styles['App']}>
+            <header className={styles['App-header']}>
+              <Button colorScheme="blue">Hello world!</Button>
+            </header>
+          </div>
+        );
+}
+
+export default App;
